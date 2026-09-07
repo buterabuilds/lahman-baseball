216b9402898a66c41a5a80a0eaceabba724fd4d8
buterabuilds <mwbutera@gmail.com>
2026-09-07 18:03:45 -0500

    scripts update

--- baseball_scripts.sql
+++ baseball_scripts.sql
@@ -297,3 +297,11 @@ WHERE (po_year - yearid) < 5 AND (yearid - po_year) < 5;
 
 -- OEQ-3: It is thought that since left-handed pitchers are more rare, causing batters to face them less often, that they are more effective. Investigate this claim and present evidence to either support or dispute this claim. 
 -- First, determine just how rare left-handed pitchers are compared with right-handed pitchers. Are left-handed pitchers more likely to win the Cy Young Award? Are they more likely to make it into the hall of fame?
+SELECT people.throws,
+	ROUND(AVG(pitching.era::numeric), 3) AS avg_era,
+	ROUND(AVG(((pitching.h + pitching.bb) / NULLIF(pitching.ipouts / 3.0, 0))::numeric), 3) AS avg_whip
+FROM pitching
+JOIN people
+	ON people.playerid = pitching.playerid
+WHERE people.throws IN ('L','R')
+GROUP BY people.throws;
